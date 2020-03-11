a9b564f149c2b80c6f20160d6bca9a7f5f6f332f
Thomas Rainford <thomas@rainfords.net>
2020-03-11 14:29:01 +1300

    Main page is linked to both algorithm pages

--- app.js
+++ app.js
@@ -5,9 +5,16 @@ const app = express();
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/pathfinding.html");
+    res.sendFile(__dirname + "/index.html");
 });
 
+app.get("/pathfinding", function (req, res) {
+    res.sendFile(__dirname + "/pathfinding.html")
+});
+
+app.get("/sorting", function (req, res) {
+    res.sendFile(__dirname + "/sorting.html")
+});
 
 app.listen(3000, function () {
     console.log("Server Running");

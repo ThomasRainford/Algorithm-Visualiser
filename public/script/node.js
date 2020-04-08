b47434a5aa9b0eab36c635654e587ef539bdb197
Thomas Rainford <thomas@rainfords.net>
2020-04-08 14:34:38 +1200

    Refactor index.js to use object in different files

---
+++ public/script/node.js
@@ -0,0 +1,14 @@
+/**
+* Create a node in a position in the grid.
+*
+* @param row
+* @param col
+* @param weight
+* @param state
+*/
+export function Node(row, col, weight, state) {
+    this.row = row;
+    this.col = col;
+    this.weight = weight;
+    this.state = state;
+}
